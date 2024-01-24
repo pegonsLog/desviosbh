56174c9dfcdf3491eadac20fbf59db307ca2e90e
pegonsLog <pegons.log@gmail.com>
2024-01-24 20:56:47 -0300

    Casa - 24/01/2024

--- src/app/desvios/desvios-list/desvios-list.component.ts
+++ src/app/desvios/desvios-list/desvios-list.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,7 +12,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router, RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Desvio } from '../../model/desvio';
 
 @Component({
@@ -42,10 +42,10 @@ export class DesviosListComponent {
   private firestore: Firestore = inject(Firestore);
   desviosFire$: Observable<any>;
 
-  queryField = new FormControl();
+  queryField: string = '';
   value: string = '';
-  regionais: string[] = [];
-  regional: string = 'GERAL';
+  // regionais: string[] = [];
+  // regional: string = 'GERAL';
   contador: number = 0;
   bloco: any;
   // itemsRef: AngularFireList<any>;
@@ -57,17 +57,11 @@ export class DesviosListComponent {
     const desviosCollection = collection(this.firestore, 'desvios');
 
     this.desviosFire$ = collectionData(desviosCollection) as Observable<Desvio[]>;
-    // this.desviosFire$ = this.desviosService
-    //   .list()
-    //   .pipe(
-    //     map((result: any) =>
-    //       result.sort((a: any, b: any) => a.linha.localeCompare(b.linha))
-    //     )
-    //   );
-    // this.desviosService
-    //   .list()
-    //   .pipe(tap((desvios: Desvio[]) => (this.contador = desvios.length)))
-    //   .subscribe();
+    this.desviosFire$.pipe(
+        map((result: any) =>
+          result.sort((a: any, b: any) => a.linha.localeCompare(b.linha))
+        )
+      );
   }
 
   blocos() {
@@ -78,19 +72,20 @@ export class DesviosListComponent {
   }
 
   onSearch() {
-    // let value = this.queryField.value;
-    // if (value && (value = value.trim()) !== '') {
-    //   this.desviosFire$ = this.desviosService.listFire().pipe(
-    //     map((desvios) =>
-    //       desvios.filter((desvio: any) =>
-    //         desvio.linha.includes(value.toUpperCase())
-    //       )
-    //     ),
-    //     map((result) =>
-    //       result.sort((a: any, b: any) => a.linha.localeCompare(b.linha))
-    //     ),
-    //     tap((desvios: Desvios) => (this.contador = desvios.length))
-    //   );
-    // }
+    let value = this.queryField;
+console.log(value);
+    if (value && (value = value.trim()) !== '') {
+      this.desviosFire$.pipe(
+        map((desvios) =>
+          desvios.filter((desvio: any) =>
+            desvio.linha.includes(value.toUpperCase())
+          )
+        ),
+        // map((result) =>
+        //   result.sort((a: any, b: any) => a.linha.localeCompare(b.linha))
+        // ),
+        // tap((desvios: Desvios) => (this.contador = desvios.length))
+      );
+    }
   }
 }
